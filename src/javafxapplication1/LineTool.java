298fa191178b01e1b6d51ef3566418518d52b432
Carmine Cavaliere <cassdfa@gmail.com.com>
2025-01-12 16:39:14 +0100

    inizializzazione interfaccia

---
+++ src/javafxapplication1/LineTool.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafxapplication1;
+import javafx.scene.Node;
+
+/**
+ * 
+ *
+ * @author cassd
+ */
+public class LineTool {
+
+    public LineTool() {
+    }
+    
+    
+    
+}
