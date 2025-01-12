3e1d0ddce5c5212ada25579bf399c6a8487d068f
vinjs <vinjs@host.docker.internal>
2025-01-13 00:17:31 +0100

    bug risolti linea

--- src/javafxapplication1/FXMLDocumentController.java
+++ src/javafxapplication1/FXMLDocumentController.java
@@ -2,132 +2,59 @@ package javafxapplication1;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.paint.Color;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.shape.Shape;
-import javafx.scene.shape.*;
+import javafx.scene.input.MouseEvent;
 
 public class FXMLDocumentController implements Initializable {
-    
-    @FXML
-    private Label label;
-    
-    // Variabili di stato
-    private boolean isLineMode = false;   
-    private boolean isFirstClick = true;  
-    private double startX, startY; 
-    private GraphicsContext gc;
-    private Line linea;
-    
+
     @FXML
     private AnchorPane anchorPanePaper;
-    
-    
-    @FXML
-    private Button lineButton;       
-    
-    
-    
-    @FXML
-    private void handleLineButtonAction(ActionEvent event) {
-        
-        isLineMode = true;
-        isFirstClick = true;
-        System.out.println("Modalità Line attivata.");
 
-        
-        
-    }
-    
     @FXML
-    private void onMousePressedPaper(javafx.scene.input.MouseEvent event){
-        if (isLineMode) {
-          this.startX = event.getX();
-          this.startY = event.getY();
-          Line newlinea = new Line();
-          this.linea = newlinea;
-          this.linea.setStartX(this.startX);
-          this.linea.setStartX(this.startY);
-          //this.linea.setEndX(this.startX);
-          //this.linea.setEndY(this.startY);
-          this.linea.setStroke(Color.BLUE);
-          this.linea.setStrokeWidth(3);
-          //this.anchorPanePaper.getChildren().add(linea);
-          System.out.println("Posizione iniziale settata");  
-        }
-        
-        
-    }
+    private ColorPicker colorPickerLine;
+
+    private LineTool lineTool;
     
-    
-    @FXML
-    private void onMouseDraggedPaper(javafx.scene.input.MouseEvent event){
-        if (isLineMode) {
-            double startXX = event.getX();
-            double startYY = event.getY();
-            
-            this.linea.setEndX(startXX);
-            this.linea.setEndY(startYY);
-            this.anchorPanePaper.getChildren().add(this.linea);
-            //drawingCanvas.getGraphicsContext2D().strokeLine(this.startX, this.startY, startXX, startYY);
-          
-        } 
-        
-    }
-    
-    @FXML
-    private void onMouseReleasedPaper(javafx.scene.input.MouseEvent event){
-        if (isLineMode) {
-            double endX = event.getX();
-            double endY = event.getY();
-            this.linea.setEndX(endX);
-            this.linea.setEndY(endY);
-            
-               
-        }
-        
-        
-    }
-   
-    @FXML
+    private boolean lineMode = false;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
- 
-        this.anchorPanePaper.setOnMousePressed(this::onMousePressedPaper);
-        this.anchorPanePaper.setOnMouseDragged(this::onMouseDraggedPaper);
-        this.anchorPanePaper.setOnMouseReleased(this::onMouseReleasedPaper);
+        
+        lineTool = new LineTool(anchorPanePaper);
+
+        
+        anchorPanePaper.setOnMousePressed(lineTool::onMousePressed);
+        anchorPanePaper.setOnMouseDragged(lineTool::onMouseDragged);
+        anchorPanePaper.setOnMouseReleased(lineTool::onMouseReleased);
+
+        
+        colorPickerLine.setOnAction(event -> {
+            lineTool.setLineColor(colorPickerLine.getValue());
+        });
     }
 
-    /*
-    private void handleCanvasClick(javafx.scene.input.MouseEvent event) {
-    if (isLineMode) {
-        if (isFirstClick) {
-            
-            startX = event.getX();
-            startY = event.getY();
-            isFirstClick = false;
-        }
-        else{
-            
-            double endX = event.getX();
-            double endY = event.getY();
-
-            
-            drawingCanvas.getGraphicsContext2D().strokeLine(startX, startY, endX, endY);
-
-            
-            isLineMode = false;
-            }
-        }
+    @FXML
+    private void handleLineButtonAction() {
+        lineMode = true;
+        System.out.println("Strumento Linea attivato.");
     }
-*/
-     
     
+    @FXML
+    private void onMousePressedPaper(MouseEvent event) {
+        lineTool.onMousePressed(event);
+    }
+
+    @FXML
+    private void onMouseDraggedPaper(MouseEvent event) {
+        lineTool.onMouseDragged(event);
+    }
+
+    @FXML
+    private void onMouseReleasedPaper(MouseEvent event) {
+        lineTool.onMouseReleased(event);
+    }
+
 }
--- src/javafxapplication1/LineTool.java
+++ src/javafxapplication1/LineTool.java
@@ -1,10 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package javafxapplication1;
 import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 
 /**
  * 
@@ -12,10 +12,42 @@ import javafx.scene.Node;
  * @author cassd
  */
 public class LineTool {
-
-    public LineTool() {
+    
+    private Line currentLine;
+    private Color lineColor;
+    private Pane drawingPane;
+    
+    public LineTool(Pane drawingPane) {
+        this.drawingPane = drawingPane;
+        this.lineColor = Color.BLACK; // Default color
     }
-    
-    
-    
+
+    public void setLineColor(Color color) {
+        this.lineColor = color;
+    }
+
+    public void onMousePressed(MouseEvent event) {
+        currentLine = new Line();
+        currentLine.setStartX(event.getX());
+        currentLine.setStartY(event.getY());
+        currentLine.setStroke(lineColor);
+        currentLine.setStrokeWidth(3);
+        drawingPane.getChildren().add(currentLine);
+    }
+
+    public void onMouseDragged(MouseEvent event) {
+        if (currentLine != null) {
+            currentLine.setEndX(event.getX());
+            currentLine.setEndY(event.getY());
+        }
+    }
+
+    public void onMouseReleased(MouseEvent event) {
+        if (currentLine != null) {
+            currentLine.setEndX(event.getX());
+            currentLine.setEndY(event.getY());
+            currentLine = null; 
+        }
+    }
+   
 }
