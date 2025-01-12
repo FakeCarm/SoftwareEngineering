be79018f7a2f76e779bed0877a2b63fb60fcbf59
vinjs <vinjs@host.docker.internal>
2025-01-12 17:04:07 +0100

    inserimento linea

--- src/javafxapplication1/FXMLDocumentController.java
+++ src/javafxapplication1/FXMLDocumentController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javafxapplication1;
 
 import java.net.URL;
@@ -10,26 +5,40 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
 
-/**
- *
- * @author cassd
- */
 public class FXMLDocumentController implements Initializable {
     
     @FXML
     private Label label;
-    
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("CIao mondo!");
+    private Canvas drawingCanvas;    
+
+    @FXML
+    private Button lineButton;       
+    
+    
+    // Questo metodo è legato a onAction="#handleLineButtonAction"
+    @FXML
+    private void handleLineButtonAction(ActionEvent event) {
+        
+        GraphicsContext gc = drawingCanvas.getGraphicsContext2D();
+
+        gc.setStroke(Color.BLACK);
+
+        gc.strokeLine(10, 10, 200, 200);
+        
+        System.out.println("Linea disegnata!");
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        // Inizializza qui se hai bisogno di logica di start-up
     }    
     
 }
