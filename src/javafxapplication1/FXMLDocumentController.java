5d3f5525aaf5ddf36d3b55c389911b60c1195f41
Carmine Cavaliere <cassdfa@gmail.com.com>
2025-01-12 19:54:39 +0100

    Aggiunta classe Line Eliminazione Canvas BUG: Posizione iniziale linea

--- src/javafxapplication1/FXMLDocumentController.java
+++ src/javafxapplication1/FXMLDocumentController.java
@@ -10,19 +10,26 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Shape;
+import javafx.scene.shape.*;
 
 public class FXMLDocumentController implements Initializable {
     
     @FXML
     private Label label;
-
-    @FXML
-    private Canvas drawingCanvas;    
+    
     // Variabili di stato
     private boolean isLineMode = false;   
     private boolean isFirstClick = true;  
-    private double startX, startY;        
-
+    private double startX, startY; 
+    private GraphicsContext gc;
+    private Line linea;
+    
+    @FXML
+    private AnchorPane anchorPanePaper;
+    
+    
     @FXML
     private Button lineButton;       
     
@@ -34,14 +41,71 @@ public class FXMLDocumentController implements Initializable {
         isLineMode = true;
         isFirstClick = true;
         System.out.println("Modalità Line attivata.");
-    }
 
+        
+        
+    }
+    
+    @FXML
+    private void onMousePressedPaper(javafx.scene.input.MouseEvent event){
+        if (isLineMode) {
+          this.startX = event.getX();
+          this.startY = event.getY();
+          Line newlinea = new Line();
+          this.linea = newlinea;
+          this.linea.setStartX(this.startX);
+          this.linea.setStartX(this.startY);
+          //this.linea.setEndX(this.startX);
+          //this.linea.setEndY(this.startY);
+          this.linea.setStroke(Color.BLUE);
+          this.linea.setStrokeWidth(3);
+          //this.anchorPanePaper.getChildren().add(linea);
+          System.out.println("Posizione iniziale settata");  
+        }
+        
+        
+    }
+    
+    
+    @FXML
+    private void onMouseDraggedPaper(javafx.scene.input.MouseEvent event){
+        if (isLineMode) {
+            double startXX = event.getX();
+            double startYY = event.getY();
+            
+            this.linea.setEndX(startXX);
+            this.linea.setEndY(startYY);
+            this.anchorPanePaper.getChildren().add(this.linea);
+            //drawingCanvas.getGraphicsContext2D().strokeLine(this.startX, this.startY, startXX, startYY);
+          
+        } 
+        
+    }
+    
+    @FXML
+    private void onMouseReleasedPaper(javafx.scene.input.MouseEvent event){
+        if (isLineMode) {
+            double endX = event.getX();
+            double endY = event.getY();
+            this.linea.setEndX(endX);
+            this.linea.setEndY(endY);
+            
+               
+        }
+        
+        
+    }
+   
+    @FXML
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        drawingCanvas.setOnMouseClicked(this::handleCanvasClick);
+ 
+        this.anchorPanePaper.setOnMousePressed(this::onMousePressedPaper);
+        this.anchorPanePaper.setOnMouseDragged(this::onMouseDraggedPaper);
+        this.anchorPanePaper.setOnMouseReleased(this::onMouseReleasedPaper);
     }
 
-    
+    /*
     private void handleCanvasClick(javafx.scene.input.MouseEvent event) {
     if (isLineMode) {
         if (isFirstClick) {
@@ -49,7 +113,8 @@ public class FXMLDocumentController implements Initializable {
             startX = event.getX();
             startY = event.getY();
             isFirstClick = false;
-        } else {
+        }
+        else{
             
             double endX = event.getX();
             double endY = event.getY();
@@ -62,6 +127,7 @@ public class FXMLDocumentController implements Initializable {
             }
         }
     }
-
+*/
+     
     
 }
