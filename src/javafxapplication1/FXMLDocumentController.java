c31173b4183184aded2629a65efd825013d95e4c
Carmine Cavaliere <cassdfa@gmail.com.com>
2025-01-15 15:53:45 +0100

    Revert "Pulizia codice"
    This reverts commit 35431d6806f653f4e5b022b887904eb193934c2c.

--- src/javafxapplication1/FXMLDocumentController.java
+++ src/javafxapplication1/FXMLDocumentController.java
@@ -12,35 +12,37 @@ import javafx.scene.shape.Line;
 import javafx.scene.shape.Shape;
 
 public class FXMLDocumentController implements Initializable {
-    
-    // --------- FXML VARIABILI ------------------
+
     @FXML
     private AnchorPane anchorPanePaper;
     @FXML
     private AnchorPane anchorPaneBar;
     
     
-            // SELETTORE DI COLORI
+    // SELETTORE DI COLORI
     @FXML
     private ColorPicker colorPickerStroke;
     @FXML
     private ColorPicker colorPickerFill;
-    // -------------------------------------------
-    
-    
     
     private Paper drawingPaper; 
-    
+    // FORME GEOMETRICHE
+    //private LineTool lineTool;
+    //private RectangleTool rectangleTool;
     
     
     // STATI SISTEMA
     private ToolState state;
-   
+    
+    //private boolean lineMode = false;
+    //private boolean rectangleMode = false;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         
         this.drawingPaper = new Paper(this.anchorPanePaper);
         
+
         // Aggiunta del Listener sui ColorPicker per aggiornamento automatico del colore quando si rimane selezionati su una forma
         colorPickerStroke.valueProperty().addListener((observable, oldValue, newValue) -> {
             if (state instanceof SelectedShapeTool) {
@@ -57,20 +59,35 @@ public class FXMLDocumentController implements Initializable {
         
     }
 
-      
-    // Gestione azioni interfaccia
+    
+    /*
+    @FXML
+    private void handleLineButtonAction() {
+        lineMode = true;
+        rectangleMode = false;
+        System.out.println("Strumento Linea attivato.");
+    }
+    */
+    
+    
+    // BOTTONI. La pressione del bottone imposta lo stato del sistema che deve lavorare.
     @FXML
     public void handleLineButtonAction() {
-        this.state = new LineTool(drawingPaper, anchorPaneBar,colorPickerStroke.getValue(),colorPickerFill.getValue()); 
+        this.state = new LineTool(drawingPaper, anchorPaneBar,colorPickerStroke.getValue(),colorPickerFill.getValue());
+        state = new LineTool(drawingPaper, anchorPaneBar, colorPickerStroke.getValue(), colorPickerFill.getValue());  
         System.out.println("Strumento Linea attivato."); 
     }
     
     @FXML
     public void handleRectangleButtonAction() {
         state = new RectangleTool(drawingPaper, anchorPaneBar, colorPickerStroke.getValue(), colorPickerFill.getValue());
-        System.out.println("Strumento Rettangolo attivato.");
-    }
 
+
+        System.out.println("Strumento Rettangolo attivato.");
+}
+    
+    
+    
     @FXML 
     public void colorPickerStrokeAction(){
         System.out.println("Strumento Colore Stroke Selezionato");
@@ -83,7 +100,7 @@ public class FXMLDocumentController implements Initializable {
     }
     
     
-    // PRESSIONE SU FOGLIO DA DISEGNO SUL FOGLIO DA DISEGNO
+    // MOUSE SUL FOGLIO DA DISEGNO
     @FXML
     public void onMousePressedPaper(MouseEvent event) {
         
@@ -107,4 +124,105 @@ public class FXMLDocumentController implements Initializable {
         }
     }
     
+    
+    
+    
+    
+    
+    // COLOR PICKER
+    /*
+    @FXML 
+    private void colorPickerStrokeAction(){
+        Color colorStroke = colorPickerStroke.getValue();
+        
+        if (colorStroke != null){
+            System.out.println("Colore Stroke Selezionato " + colorStroke.toString());
+        }
+        else{
+            System.out.println("STROKE NULLO ");
+        }
+        if (this.state != null){
+            state.setStrokeColor(colorStroke);
+        }
+    }
+    
+    @FXML 
+    private void colorPickerFillAction(){
+        Color colorFill = colorPickerFill.getValue();
+        if (colorFill != null){
+            System.out.println("Colore Fill Selezionato " + colorFill.toString());
+        }
+        else{
+            System.out.println("FILL NULLO ");
+        }
+        
+        
+        
+        
+    }
+    */
+    
+    
+    /*
+    @FXML
+    priv@FXML
+    private void handleRectangleButtonAction() {
+        this.state = new RectangleTool(anchorPanePaper);
+        
+    }ate void handleRectangleButtonAction() {
+        rectangleMode = true;
+        lineMode = false;
+        System.out.println("Strumento Rettangolo attivato.");
+    }
+    */
+    
+    
+    /*
+    @FXML
+    private void onMousePressedPaper(MouseEvent event) {
+        if (lineMode == true && rectangleMode == false){
+            lineTool.onMousePressed(event);
+        }
+        
+        if (lineMode == false && rectangleMode == true){
+            rectangleTool.onMousePressed(event);
+        }
+        
+    }
+    */
+    
+    
+    
+    
+    
+    
+    
+    // VECCHI ON MOUSE PRESSED/DRAGGED
+/*
+    @FXML
+    private void onMouseDraggedPaper(MouseEvent event) {
+        if (lineMode == true && rectangleMode == false){
+            lineTool.onMouseDragged(event);
+        }
+        
+        if (lineMode == false && rectangleMode == true){
+            rectangleTool.onMouseDragged(event);
+        }
+        
+    }
+    */
+    
+    
+     /*
+    @FXML
+    private void onMouseReleasedPaper(MouseEvent event) {
+        if (lineMode == true && rectangleMode == false){
+            lineTool.onMouseReleased(event);
+        }
+        
+        if (lineMode == false && rectangleMode == true){
+            rectangleTool.onMouseReleased(event);
+        }
+    }
+    */
 }
--- src/javafxapplication1/LineTool.java
+++ src/javafxapplication1/LineTool.java
@@ -1,8 +1,12 @@
 package javafxapplication1;
+import javafx.scene.Node;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.layout.AnchorPane;
+import javafxapplication1.Invoker;
+import javafxapplication1.AddShape;
 /**
  * 
  *
@@ -11,17 +15,53 @@ import javafx.scene.layout.AnchorPane;
 public class LineTool extends SelectedShapeTool{
     
     public Line currentLine;
-    private final Paper anchorPanePaper;
+    private Paper anchorPanePaper;
     public int count = 0;
     
-          
+    /*
+     public LineTool(Pane drawingPane, Color lineStrokeColor) {
+        this.drawingPane = drawingPane;
+        if (lineStrokeColor != null){
+            this.lineStrokeColor = lineStrokeColor;
+        }
+        else{
+            this.lineStrokeColor = Color.BLACK; // Default color
+        }
+    */
+   
+        
      public LineTool(Paper anchorPanePaper, AnchorPane anchorPaneBar, Color strokeColor, Color fillColor) {
         super(strokeColor,fillColor);
         this.anchorPanePaper = anchorPanePaper;  
         super.anchorPaneBar = anchorPaneBar;
 
     }
+    /*
+    public void setLineColor(Color color) {
+        this.lineColor = color;
+    }
+    */
     
+    
+    /*
+     @Override
+    public void onMousePressed(MouseEvent event) {
+        currentLine = new Line();
+        currentLine.setId("line"+ (count++));
+        currentLine.setStartX(event.getX());
+        currentLine.setStartY(event.getY());
+        currentLine.setEndX(event.getX());
+        currentLine.setEndY(event.getY());
+        Color color = super.getStrokeColorProperty();
+        if (color != null){
+            currentLine.setStroke(super.getStrokeColorProperty());
+            System.out.println("COLORE AGGIUNTO");
+        }
+        
+        currentLine.setStrokeWidth(10);
+        anchorPanePaper.getChildren().add(currentLine);
+    }
+     */
     @Override
     public void onMousePressed(MouseEvent event) {
         currentLine = new Line();
@@ -41,6 +81,7 @@ public class LineTool extends SelectedShapeTool{
         }
     }
 
+
     @Override
     public void onMouseDragged(MouseEvent event) {
         if (currentLine != null) {
@@ -58,4 +99,15 @@ public class LineTool extends SelectedShapeTool{
             currentLine = null; 
         }
     }
+    
+    /*
+    
+     @Override
+    public void setStrokeColor(Color color){
+        this.lineStrokeColor = color;
+    }
+    
+    */
+   
+   
 }
